4ad182e0d46439a6f322855da7a633ccf8319eda
greatshot101 <116977958+greatshot101@users.noreply.github.com>
2024-03-26 17:25:30 -0500

    Delete src/main/java/api/hbm directory

--- src/main/java/api/hbm/block/IEnterableBlock.java
+++
@@ -1,9 +0,0 @@
-package api.hbm.block;
-
-import net.minecraft.util.EnumFacing;
-import net.minecraft.world.World;
-
-public interface IEnterableBlock {
-    public boolean canItemEnter(World world, int x, int y, int z, EnumFacing dir, IConveyorItem entity);
-    public void onItemEnter(World world, int x, int y, int z, EnumFacing dir, IConveyorItem entity);
-}
